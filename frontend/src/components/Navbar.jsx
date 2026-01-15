35eadbd5786bde8039fd69ca2d409f0b027792c5
Moses_247 <asugam9@gmail.com>
2026-01-15 15:18:02 +0300

    close icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -61,7 +61,16 @@ const Navbar = () => {
 
         {/* SIDEBAR menu for small screens */}
         <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full': 'w-0'}`}>
-                
+                <div className='flex flex-col text-gray-600'>
+                    <div onClick={() =>setVisible(false)} className="flex items-center gap-4 p-3">
+                        <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
+                        <p>Back</p>
+                    </div>
+                    <NavLink className='py-2 pl-6 border' to='/'>HOME</NavLink>
+                    <NavLink className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+                    <NavLink className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+                    <NavLink className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+                </div>
         </div>
     
     </div>
