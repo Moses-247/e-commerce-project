6a0c4768b592c842367276b35d2635bbc1cc028e
Moses_247 <asugam9@gmail.com>
2026-01-27 23:25:05 +0300

    best Sellers

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -19,8 +19,8 @@ const LatestCollection = () => {
       <div className="my-10">
         <div className="text-center py-8 text-3xl">
           <Title text1={'LATEST'} text2={'COLLECTION'} />
-          <p className="w3/4 m-auto text-xs sm:text-sm md:text-base text-grey-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, sunt consequuntur. Iusto eos distinctio dignissimos eum blanditiis itaque provident assumenda illo adipisci tenetur, velit nisi laborum perferendis quam repellendus obcaecati?
+          <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-grey-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, sunt consequuntur.
           </p>
         </div>
 
@@ -28,7 +28,7 @@ const LatestCollection = () => {
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-col-4 lg:grid-5 gap-4 gap-y-6">
           {
             latestProducts.map((item, index) =>(
-              <ProductItem key={index} id={item.id} image={item.image} name={item.name} price={item.price}/>
+              <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
             ))
           }
         </div>
