83ddacbec031790e504046b81d7ad464ecc88305
Moses_247 <asugam9@gmail.com>
2026-01-28 13:59:04 +0300

    collection page

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,15 +1,33 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { ShopContext } from '../context/ShopContext'
 
 const Collection = () => {
 
   const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
       
       {/*  Filter Options */}
-      <div className=""></div>
+      <div className="min-w-60">
+        <p className='my-2 text-xl flex item-center cursor-pointer gap-2'>FILTERS</p>
+        {/* Category Filter */}
+        <div className={`border border-grey-300 pl-5 py-3 mt-6 ${showFilter ? '' :'hidden'} sm:block`}>
+          <p className=' mb-3 text-sm font-medium'>CATEGORIES</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-grey-700">
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" name="" id="" value={'Men'}/>Men
+            </p>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" name="" id="" value={'Women'}/>Women
+            </p>
+            <p className='flex gap-2'>
+              <input className='w-3' type="checkbox" name="" id="" value={'Kids'}/>Kids
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
